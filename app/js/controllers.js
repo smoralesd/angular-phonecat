8add51d96d2754f1058396107dbba3638e43464a
Sebastian Morales <smorales@dena.jp>
2015-12-09 14:20:06 -0300

    add base controller, use it on the html, add tests

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,20 @@
 'use strict';
 
 /* Controllers */
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function($scope) {
+
+    $scope.name = "World";
+
+    $scope.phones = [{
+        'name': 'Nexus S',
+        'snippet': 'Fast just got gaster with Nexus S'
+    }, {
+        'name': 'Motorola XOOM™ with Wi-Fi',
+        'snippet': 'The Next, Next Generation tablet.'
+    }, {
+        'name': 'MOTOROLA XOOM™',
+        'snippet': 'The Next, Next Generation tablet.'
+    }];
+});
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,11 +1,22 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
+describe('PhoneListCtrl', function() {
 
-describe('controllers', function() {
+    beforeEach(module('phonecatApp'));
 
-  it("should do something", function() {
+    it('should create "phones" model with 3 phones', inject(function($controller) {
+        var scope = {};
+        var ctrl = $controller('PhoneListCtrl', {$scope: scope});
 
-  });
+        expect(scope.phones.length).toBe(3);
+    }));
 
+    it('should have a name property with value "World"', inject(function($controller) {
+        var scope = {};
+        var ctrl = $controller('PhoneListCtrl', {$scope: scope});
+
+        expect(scope.name).toBe("World");
+    }));
 });
+
